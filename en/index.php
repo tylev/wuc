84bbcbd093e516d23611cda8665e7ad657919bed
sunnankar <sunnankar@users.noreply.github.com>
2015-04-06 15:57:58 -0400

    Update index.php
    Rearrange analytics code.

--- en/index.php
+++ en/index.php
@@ -34,6 +34,19 @@
 
 <script src="/data_v2/js/vendor/modernizr-2.6.2.min.js"></script>
 
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-22253606-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 
 </head>
 <body>
@@ -362,18 +375,5 @@
 <script type="text/javascript">stLight.options({publisher: "d7d6a2bd-5e43-4821-807e-0ce5c84f0862", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
 <script type="text/javascript" src="/mc/js/mailing-list.js"></script>
 
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-22253606-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
 </body>
 </html>
